10a81bb61b76059f0b99b28dbe797e4e2be6512c
Seba Gines <sebastian.gines@gmail.com>
2023-01-26 21:00:38 -0300

    Adding example of class

--- clase5/js/script.js
+++ clase5/js/script.js
@@ -1,33 +1,60 @@
 
-// Objects
+// Exercise
 
-let car_one = {
-    color: "red",
-    brand: "Fiat",
-    owner: "Pep",
-    patent: "ABC123" 
+class Product {
+    // Properties
+    constructor( name, price, stock ){
+        this.name = name,
+        this.price = price,
+        this.stock = stock
+    }
+
+    // Methods
+    get_data(){
+        console.log( "======= Product =========" );
+        console.log( "Name:", this.name );
+        console.log( "Price:", this.price );
+        console.log( "Stock:", this.stock );
+        console.log( "" );
+    }
+
+    get_stock(){
+        return (this.stock > 0);
+    }
+
+    set_sell( stock ){
+        if ( typeof(stock) == "number" && stock >= 0 && this.stock >= stock ){
+            this.stock = this.stock - stock;
+            console.log( "Thanks for your purchase!" )
+        } else {
+            console.log( "No stock for this product!" )
+        }
+    }
+
+    sell(){
+        if ( this.stock > 0 ){
+            this.stock --;
+        }
+    }
+
 }
 
-let car_two = {
-    color: "yellow",
-    brand: "Ferrari",
-    owner: "Luisa",
-    patent: "TRE142" 
+/***********************************************************************************/
+// Products
+/***********************************************************************************/
+let product1 = new Product( "Lamp", 5000, 5 );
+let product2 = new Product( "Monitor", 15000, 3 );
+let product3 = new Product( "Mouse", 9000, 7 );
+
+product1.get_data();
+product2.get_data();
+product3.get_data();
+
+// Sell
+
+let product_user = "Lamp";
+if( product1.get_stock() ){
+    let quantity = parseInt(prompt("How many units do you need?"))
+    product1.set_sell(quantity);
+    product1.get_data();
 }
-
-console.log( car_one );
-console.log( "Car Data" );
-console.log( "Color:", car_one.color );
-console.log( "brand:", car_one.brand );
-
-console.log( car_two );
-console.log( "Car Data" );
-console.log( "Color:", car_two.color );
-console.log( "brand:", car_two.brand );
-
-// Constructor function 
-function student ( name, last_name, dni, file ){
-
-}
-
-let student = new student( "Pep", "Lopez", 32454123, "AXQ941" )
